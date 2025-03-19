c0a9dd9cf8830705fd6890803ab07f758fb0bb80
Arjan Hoek <arjanhoek5191@gmail.com>
2025-03-19 21:08:38 +0100

    update text

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! This is a new version';
+    return 'Hello World! This is the very latest version...';
   }
 
   getGoodbye(): string {
