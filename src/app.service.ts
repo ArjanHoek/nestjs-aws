15cc5c81613c30ecc492a1a585ef738c4f3c95cd
Arjan Hoek <arjanhoek5191@gmail.com>
2025-03-20 07:52:15 +0100

    create index page with styles

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World! This is the very latest version...';
-  }
-
-  getGoodbye(): string {
-    return 'Goodbye World!';
-  }
-}
+export class AppService {}
